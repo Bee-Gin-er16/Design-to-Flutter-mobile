552cb8757a78953dc4e66de16c062b9dc67040c6
Bee-Gin-er16 <hanseubertdelacruz@gmail.com>
2025-03-25 14:08:14 +0800

    initial widget
    appbar

--- user_interface/lib/pages/home.dart
+++ user_interface/lib/pages/home.dart
@@ -9,11 +9,28 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
+  @override
+  Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: appBar(),
+        backgroundColor: Colors.greenAccent,
+      );
+  }
 
-  );
-}
+  AppBar appBar() {
+    return AppBar(
+      title: Text(
+        'This is appbar',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold
+        ),
+      ),
+      backgroundColor: const Color.fromARGB(255, 88, 209, 104),
+      elevation: 0.0,
+      centerTitle: true,
+    );
+  }
 
 }
